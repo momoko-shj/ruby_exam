6bb78ceb5b57b4fb8e4e60428de925ae3f87e80a
EC2 Default User <107891289+momoko-shj@users.noreply.github.com>
2022-06-27 21:57:15 +0000

    [finish]lesson8 FizzBuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,23 @@
+def fizz_buzz(input)
+  if input % 15 ==0
+   puts "FizzBuzz"
+   elsif input % 3 ==0
+   puts "Fizz"
+   elsif input % 5 ==0
+   puts "Buzz"
+   else
+   puts input.to_s
+   end
+end
+
+puts "数字を入力してください"
+
+input = gets.to_i
+
+puts "結果は・・・"
+puts fizz_buzz(input)
+
+#　質問事項　defのnumberとgetsで入力するinputの表記が一致しなくてもプログラムが作動する理由は？inputに認しています。
+#　質問事項　if、elsif　elseの出力にputsが省略されている理由はについて　入れなくてもOK？
+
+  
