35cc2464864e05bf9b6b43514b52fdf974cb2553
EC2 Default User <107891289+momoko-shj@users.noreply.github.com>
2022-06-27 23:34:33 +0000

    [start]lesson9

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -17,7 +17,7 @@ input = gets.to_i
 puts "結果は・・・"
 puts fizz_buzz(input)
 
-#　質問事項　defのnumberとgetsで入力するinputの表記が一致しなくてもプログラムが作動する理由は？inputに認しています。
-#　質問事項　if、elsif　elseの出力にputsが省略されている理由はについて　入れなくてもOK？
+#　質問事項　defのnumberとgetsで入力するinputの表記が一致しなくてもプログラムが作動する理由は？inputで統一しても作動を確認済み。
+#　質問事項　if、elsif　elseの出力にputsが省略されている理由　putsの省略はできるのか？
 
   
